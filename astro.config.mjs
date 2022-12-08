74782a3b0eec44e7f1bf2ceb7389c3c0972dd77c
webapis <60083568+webapis@users.noreply.github.com>
2022-12-08 10:26:50 +0500

    c

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
+// https://astro.build/config/
 export default defineConfig({
   integrations: [react()],
   output: "server",
